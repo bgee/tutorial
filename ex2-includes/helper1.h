bf9f16f5a8a54189b5105b12af7638219dad13ea
Can Erdogan <canerdogan.89@gmail.com>
2013-06-05 13:19:10 -0400

    Created the first two examples

---
+++ ex2-includes/helper1.h
@@ -0,0 +1,10 @@
+/**
+ * @file helper1.h
+ * @author Can Erdogan
+ * @date June 05, 2013
+ * @brief This is the first header that the main.cpp should include.
+ */
+
+#pragma once
+
+const int helper1Const = 100;
---
+++ ex2-includes/subfolder/helper2.h
@@ -0,0 +1,14 @@
+/**
+ * @file helper2.h
+ * @author Can Erdogan
+ * @date June 05, 2013
+ * @brief This is the second header that the main.cpp should include.
+ */
+
+#pragma once
+
+/// Returns an integer
+int helper2Func () {
+	return 42;
+}
+
