98904d1adcd5363ede0f061064e782bff0490600
Can Erdogan <canerdogan.89@gmail.com>
2013-06-06 18:43:42 -0400

    Started writing the second example for runtime

---
+++ runtime/ex2.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file ex2.cpp
+ * @author Can Erdogan
+ * @date June 06, 2013
+ * @brief This file is the second example of runtime errors, specifically focusing on asserting
+ * errors, infinite loops and stack overflows.
+ */
+
+#include <stdlib.h>
+#include <iostream>
+#include <Eigen/Dense>
+
+using namespace std;
+using namespace Eigen;
+
+int main () {
+}
