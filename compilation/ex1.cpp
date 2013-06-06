5bc6d6ececa57658b010ad4a603d52579b7be7cf
Can Erdogan <canerdogan.89@gmail.com>
2013-06-06 06:55:54 -0400

    Created the first example

--- compilation/ex1.cpp
+++ compilation/ex1.cpp
@@ -6,27 +6,47 @@
  * problems.
  */
 
+#include <iostream>
+
 /// Computes the sum of a given vector of values where we assume the numbers start from 0 and increase
 /// consecutively.
-int sum (const vector<int>& values) {
+int sum (const vector<>& values) {
 
 	// Get the last value - the end of the list
 	int& end = values.back();
 
 	// Compute the sum by iterating over the values until the last one
-	int sum = 0;
+	int total = 0;
 	for(int i = 0; i < end; i++)
-		sum += i;
+		total += i;
+
+	// Return the value
+	return;
 }
 
+/// Multiplies a number by two
+void multiplyTwoNumbers (int* num, int num2) {
+  *num *= num2;
+	return num;
+}
+
+/// The main thread
 int main () {
 
 	// Compute the sum of the following values
 	vector <int> values;
-	for(size_t i = -1; i < 5; i++) values.push_back(i);
+	for(int i = -1; i < 5; i++) values.push_back(i);
+	int total = sum(values);
 	
+	// If the sum of the values is less than 10, divide by two; otherwise, multiply by two with a function call
+	if(total < 10) 
+		newTotal = total / 2;
+		printf("newTotal: %d\z", newTotal);
+	else {
+		multiplyTwoNumber(total);	
+		printf("newTotal: %d\n, total)
+	}
 
-	// 
-	
-
+	// Print an exit statement
+	std::cout << The computation is completed << std::endl;
 }
